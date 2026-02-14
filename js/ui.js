bb577c9a08a65f5e4a0413c2216e752867f00090
PaulMoralesG <pleycinder2013@hotmail.com>
2026-02-13 22:40:19 -0500

    Style: Change expense button to red when selected

--- js/ui.js
+++ js/ui.js
@@ -237,7 +237,7 @@ export function setTransactionType(type) {
     DOM.categoryGrid.style.display = 'grid'; 
     
     if(type === 'expense') {
-        DOM.btnTypeExpense.className = "px-4 py-2 rounded-lg text-sm font-extrabold shadow-sm bg-white text-black transition-all";
+        DOM.btnTypeExpense.className = "px-4 py-2 rounded-lg text-sm font-extrabold shadow-sm bg-red-100 text-red-700 transition-all";
         DOM.btnTypeIncome.className = "px-4 py-2 rounded-lg text-sm font-bold text-gray-500 hover:text-gray-700 transition-all";
         initCategoryGrid(EXPENSE_CATEGORIES);
         DOM.conceptInput.placeholder = "¿En qué lo gastaste?";
